46fef104b32d67a1afaee46df1495c496ceb7614
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-05-08 10:39:03 -0300

    add views and model

---
+++ src/main/java/Main/Flights.java
@@ -0,0 +1,12 @@
+package Main;
+
+/**
+ *
+ * @author guiespim
+ */
+public class Flights {
+
+    public static void main(String[] args) {
+        new View.Menu_GUI().setVisible(true);
+    }
+}
---
+++ src/main/java/Model/Flight_DAO.java
@@ -0,0 +1,27 @@
+package Model;
+
+/**
+ *
+ * @author guiespim
+ */
+public class Flight_DAO {
+    
+    public String destination;
+    public String origin;
+    public int number;
+    public boolean[] reservation = {
+            false, false,
+            false, false,
+            false, false
+    };
+
+    public Flight_DAO(int number, String origin, String destination) {
+        this.number = number;
+        this.origin = origin;
+        this.destination = destination;
+    }
+
+    public Flight_DAO() {
+    }
+    
+}
